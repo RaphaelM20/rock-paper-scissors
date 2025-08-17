f43b90a6a3a72c393224b6382bbd30ebe97f1bd2
Raphael Moreira <43351851+RaphaelM20@users.noreply.github.com>
2025-08-17 14:12:50 -0400

    Added logic to get computer choice
    Created funcation that randomly gets computers choice

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+function getComputerChoice () {
+    let randomNumber;
+    randomNumber = Math.random();
+
+    let computerChoice;
+
+    if (randomNumber <= 0.33) {
+        computerChoice = 'rock';
+        return computerChoice;
+    }
+
+    else if (randomNumber > 0.33 && randomNumber <= 0.66) {
+        computerChoice = 'paper';
+        return computerChoice;
+    }
+
+    else {
+        computerChoice = 'scisscors';
+        return computerChoice;
+    }
+}
+
+console.log(getComputerChoice());
